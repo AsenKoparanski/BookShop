d7a0845e18184c7427fa6e258fe9bbb68e68e51b
AsenKoparanski <asenakoparanski@gmail.com>
2019-03-13 23:54:55 +0000

    Update Book.java

--- src/asen/koparanski/model/Book.java
+++ src/asen/koparanski/model/Book.java
@@ -2,4 +2,38 @@ package asen.koparanski.model;
 
 public class Book {
 
+    private String name;
+    private int year;
+    private double price;
+
+    public Book(String name, int year, double price) {
+        this.name = name;
+        this.year = year;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
 }
