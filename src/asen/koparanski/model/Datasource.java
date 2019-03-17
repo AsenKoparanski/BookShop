1294f78d93d282bf1c305b7f7dac8def9c49ca69
AsenKoparanski <asenakoparanski@gmail.com>
2019-03-17 22:47:57 +0000

    Update Datasource.java

--- src/asen/koparanski/model/Datasource.java
+++ src/asen/koparanski/model/Datasource.java
@@ -10,6 +10,16 @@ public class Datasource {
 
     public static final String CONNECTION_STRING = "jdbc:sqlite:D:\\workspace\\BookShop\\" + DB_NAME;
 
+    public static final String TABLE_BOOKS = "books";
+    public static final String COLUMN_BOOK_ID = "id";
+    public static final String COLUMN_BOOK_TITLE = "title";
+    public static final String COLUMN_BOOK_YEAR = "year";
+    public static final String COLUMN_BOOK_PRICE = "price";
+    public static final int INDEX_BOOK_ID = 1;
+    public static final int INDEX_BOOK_TITLE = 2;
+    public static final int INDEX_BOOK_YEAR = 3;
+    public static final int INDEX_BOOK_PRICE = 4;
+
     private Connection conn;
     private static Datasource instance = new Datasource();
 
