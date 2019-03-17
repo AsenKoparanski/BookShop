3255c5a7889992f15667fed97e4ecaca027c79d2
AsenKoparanski <asenakoparanski@gmail.com>
2019-03-17 22:56:09 +0000

    Update Book.java

--- src/asen/koparanski/model/Book.java
+++ src/asen/koparanski/model/Book.java
@@ -1,49 +1,53 @@
 package asen.koparanski.model;
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class Book {
 
-    private int id;
-    private String name;
-    private int year;
-    private double price;
+    private SimpleIntegerProperty id;
+    private SimpleStringProperty name;
+    private SimpleIntegerProperty year;
+    private SimpleDoubleProperty price;
 
     public Book(int id, String name, int year, double price) {
-        this.id = id;
-        this.name = name;
-        this.year = year;
-        this.price = price;
+        this.id = new SimpleIntegerProperty();
+        this.name = new SimpleStringProperty();
+        this.year = new SimpleIntegerProperty();
+        this.price = new SimpleDoubleProperty();
     }
 
     public int getId() {
-        return id;
+        return id.get();
     }
 
     public void setId(int id) {
-        this.id = id;
+        this.id.set(id);
     }
 
     public String getName() {
-        return name;
+        return name.get();
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.name.set(name);
     }
 
     public int getYear() {
-        return year;
+        return year.get();
     }
 
     public void setYear(int year) {
-        this.year = year;
+        this.year.set(year);
     }
 
     public double getPrice() {
-        return price;
+        return price.get();
     }
 
     public void setPrice(double price) {
-        this.price = price;
+        this.price.set(price);
     }
 
 }
