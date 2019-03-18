e8204e2c106869968c89c24dbbfa4a0f440921f6
AsenKoparanski <asenakoparanski@gmail.com>
2019-03-18 00:41:39 +0000

    Update Datasource.java

--- src/asen/koparanski/model/Datasource.java
+++ src/asen/koparanski/model/Datasource.java
@@ -1,8 +1,8 @@
 package asen.koparanski.model;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Datasource {
 
@@ -20,8 +20,12 @@ public class Datasource {
     public static final int INDEX_BOOK_YEAR = 3;
     public static final int INDEX_BOOK_PRICE = 4;
 
+    public static final String QUERY_BOOKS = "SELECT * FROM " + TABLE_BOOKS;
+
     private Connection conn;
     private static Datasource instance = new Datasource();
+
+    private PreparedStatement queryBooks;
 
 
     private Datasource() {
@@ -34,6 +38,8 @@ public class Datasource {
     public boolean open() {
         try {
             conn = DriverManager.getConnection(CONNECTION_STRING);
+            queryBooks = conn.prepareStatement(QUERY_BOOKS);
+
             return true;
         } catch (SQLException e) {
             System.out.println("Couldn't connect to database: " + e.getMessage());
@@ -42,12 +48,37 @@ public class Datasource {
     }
     public void close() {
         try {
+            if (queryBooks != null) {
+                queryBooks.close();
+            }
             if (conn != null) {
                 conn.close();
             }
         } catch (SQLException e) {
             System.out.println("Couldn't close connection: " + e.getMessage());
         }
+    }
+
+    public List<Book> queryBooks() {
+
+        List<Book> books = new ArrayList<>();
+
+        try {
+            ResultSet results = queryBooks.executeQuery();
+            while (results.next()) {
+                Book book = new Book();
+                book.setId(results.getInt(INDEX_BOOK_ID));
+                book.setTitle(results.getString(INDEX_BOOK_TITLE));
+                book.setYear(results.getInt(INDEX_BOOK_YEAR));
+                book.setPrice(results.getDouble(INDEX_BOOK_PRICE));
+                books.add(book);
+            }
+            return books;
+        } catch(SQLException e) {
+            System.out.println("Couldn't retrieve books: " + e.getMessage());
+            return null;
+        }
+
     }
 
 }
