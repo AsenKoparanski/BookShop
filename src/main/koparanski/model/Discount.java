244794c107d0a49b5c6ab7109b4c2f438f1f4f81
AsenKoparanski <asenakoparanski@gmail.com>
2019-03-18 13:05:47 +0000

    Update Discount.java

--- src/main/koparanski/model/Discount.java
+++ src/main/koparanski/model/Discount.java
@@ -2,8 +2,17 @@ package main.koparanski.model;
 
 import java.util.List;
 
+/**
+ * Author: Asen Koparanski
+ * Purpose: Provide logic for the discount applied to books.
+ * Date: 17.03.2019
+ */
 public class Discount {
-
+    /**
+     * Apply discounts to a given list of books and return the final total price.
+     * @param books
+     * @return totalPrice
+     */
     public double applyDiscount(List<Book> books) {
         double totalPrice = 0.0;
         for(Book book: books) {
