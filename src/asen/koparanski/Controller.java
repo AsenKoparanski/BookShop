6304a2537d0e1df1c95299d7408a0b0a93d7a0f5
AsenKoparanski <asenakoparanski@gmail.com>
2019-03-18 11:04:17 +0000

    Update Controller.java

--- src/asen/koparanski/Controller.java
+++ src/asen/koparanski/Controller.java
@@ -11,6 +11,9 @@ import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Author: Asen Koparanski
  * Purpose:
@@ -31,13 +34,19 @@ public class Controller {
     @FXML
     public void getAllBooks() {
 
-        Task<ObservableList<Book>> task = new GetAllEmployeesTask();
+        Task<ObservableList<Book>> task = new GetAllBooksTask();
         bookTable.itemsProperty().bind(task.valueProperty());
         new Thread(task).start();
     }
+    @FXML
+    public void buyBooksButton() {
+        List<Book> books = new ArrayList<>();
+        books = bookTable.getSelectionModel().getSelectedItems();
+
+    }
 }
 // Helper class to run a background thread for a potentially longer task.
-class GetAllEmployeesTask extends Task {
+class GetAllBooksTask extends Task {
     @Override
     public ObservableList<Book> call() {
         return FXCollections.observableArrayList
