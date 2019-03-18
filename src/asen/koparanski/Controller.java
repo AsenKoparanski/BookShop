768f3b151998af102e320060d6353c0e192b833a
AsenKoparanski <asenakoparanski@gmail.com>
2019-03-18 00:41:47 +0000

    Update Controller.java

--- src/asen/koparanski/Controller.java
+++ src/asen/koparanski/Controller.java
@@ -1,6 +1,15 @@
 package asen.koparanski;
 
 
+import asen.koparanski.model.Book;
+import asen.koparanski.model.Datasource;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
 
 /**
  * Author: Asen Koparanski
@@ -10,4 +19,29 @@ package asen.koparanski;
 
 public class Controller {
 
+    @FXML
+    private BorderPane mainBorderPane;
+
+    @FXML
+    private TableView<Book> bookTable;
+
+    public void initialize() {
+        bookTable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+    }
+    @FXML
+    public void getAllBooks() {
+
+        Task<ObservableList<Book>> task = new GetAllEmployeesTask();
+        bookTable.itemsProperty().bind(task.valueProperty());
+        new Thread(task).start();
+    }
+}
+// Helper class to run a background thread for a potentially longer task.
+class GetAllEmployeesTask extends Task {
+    @Override
+    public ObservableList<Book> call() {
+        return FXCollections.observableArrayList
+                (Datasource.getInstance().queryBooks());
+
+    }
 }
